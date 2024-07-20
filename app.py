3f408263425418f43b58b8a2313ce700c0067c63
williamsagbanagba <agbanagbawilliams@gmail.com>
2024-07-20 11:37:47 -0400

    change

--- app.py
+++ app.py
@@ -56,3 +56,24 @@ Duolingo🐦https://www.duolingo.com/
 
 Github🐈‍⬛https://github.com/dashboard/
 """)
+
+# ----CONTACT ME ----
+st.header(":mailbox: Get In Touch With Me!")
+
+contact_form = """<form action="https://formsubmit.co/agbanagbawilliams@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# ---- USE LOCAL CSS FILE ----
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}<style>, unsafe_allow_html=True")
+
+
+        local_css("style/style.css.txt")
